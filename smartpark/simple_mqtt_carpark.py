a5f19a4e66b6efc0d53e1b5cccdecb44ec0c3e2e
Lili Zheng <fjzll.nju@gmail.com>
2023-11-08 14:28:47 +0800

    add the temperature value and publish through mqtt for simple_mqtt_carpark.py

--- smartpark/simple_mqtt_carpark.py
+++ smartpark/simple_mqtt_carpark.py
@@ -3,17 +3,20 @@ from datetime import datetime
 import mqtt_device
 import paho.mqtt.client as paho
 from paho.mqtt.client import MQTTMessage
+import json
 
 
 class CarPark(mqtt_device.MqttDevice):
-    """Creates a carpark object to store the state of cars in the lot"""
+    """Creates a car park object to store the state of cars in the lot"""
 
     def __init__(self, config):
         super().__init__(config)
         self.total_spaces = config['total-spaces']
         self.total_cars = config['total-cars']
+        self.client = paho.Client()
         self.client.on_message = self.on_message
         self.client.subscribe('sensor')
+        self.client.subscribe('temperature')
         self.client.loop_forever()
         self._temperature = None
 
@@ -24,25 +27,19 @@ class CarPark(mqtt_device.MqttDevice):
 
     @property
     def temperature(self):
-        self._temperature
+        return self._temperature
     
     @temperature.setter
     def temperature(self, value):
         self._temperature = value
+        self._publish_event()
         
     def _publish_event(self):
         readable_time = datetime.now().strftime('%H:%M')
-        print(
-            (
-                f"TIME: {readable_time}, "
-                + f"SPACES: {self.available_spaces}, "
-                + "TEMPC: 42"
-            )
-        )
         message = (
             f"TIME: {readable_time}, "
             + f"SPACES: {self.available_spaces}, "
-            + "TEMPC: 42"
+            + "TEMPERATURE: {self.temperature}°C"
         )
         self.client.publish('display', message)
 
@@ -50,16 +47,16 @@ class CarPark(mqtt_device.MqttDevice):
         self.total_cars += 1
         self._publish_event()
 
-
-
     def on_car_exit(self):
         self.total_cars -= 1
         self._publish_event()
 
     def on_message(self, client, userdata, msg: MQTTMessage):
         payload = msg.payload.decode()
-        # TODO: Extract temperature from payload
         # self.temperature = ... # Extracted value
+        # Extract temperature from payload
+        if msg.topic == "temperature":
+            self.temperature = float(payload)
         if 'exit' in payload:
             self.on_car_exit()
         else:
@@ -67,17 +64,10 @@ class CarPark(mqtt_device.MqttDevice):
 
 
 if __name__ == '__main__':
-    config = {'name': "raf-park",
-              'total-spaces': 130,
-              'total-cars': 0,
-              'location': 'L306',
-              'topic-root': "lot",
-              'broker': 'localhost',
-              'port': 1883,
-              'topic-qualifier': 'entry',
-              'is_stuff': False
-              }
-    # TODO: Read config from file
+    # Read config from json file  Lili Zheng
+    file_name = open('config.json', 'r')
+    config = json.load(file_name)
     car_park = CarPark(config)
-    print("Carpark initialized")
-    print("Carpark initialized")
+    print("Car park initialized")
+    file_name.close()
+
