a2c796a274f37a914f4f343877988575f73558ef
Lili Zheng <fjzll.nju@gmail.com>
2023-11-13 14:35:33 +0800

    write test case for read_temperature method in the Sensor Class

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -1,11 +1,23 @@
 import unittest
-import json
-import smartpark.simple_mqtt_sensor as ss
+from smartpark.simple_mqtt_sensor import Sensor
+from unittest.mock import MagicMock
+from smartpark.config_parser import parse_config
 
 
 class TestSensor(unittest.TestCase):
-    def test_sensor(self):
-        pass
+    def test_read_temperature(self):
+        """
+        Test the read_temperature method
+        """
+        # Mock the senseHat
+        mock_sense_hat = MagicMock()
+        # Read the configuration from config.json
+        config = parse_config('config.json')
+        sensor = Sensor(config)
+        # Set the return value for get_temperature
+        mock_sense_hat.get_temperature.return_value = 25.0
+        temperature = sensor.read_temperature()
+        self.assertEqual(temperature, 25.0)
 
 
 if __name__ == '__main__':
